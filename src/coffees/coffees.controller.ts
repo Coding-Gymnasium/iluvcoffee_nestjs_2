b8da881568cf5d5be259e26bffdffbb0e0b96d68
Nico Rithner <nicorithner@gmail.com>
2021-07-01 08:00:35 -0600

    Complete lesson 'Use route parameters'

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get('flavors')
   findAll() {
     return 'this action returns all coffees';
+  }
+
+  @Get(':id')
+  // findOne(@Param() params) {
+  //  return 'this action returns #${params.id} coffee';
+  //  }
+  findOne(@Param('id') id: string) {
+    return `this action returns #${id} coffee`;
   }
 }
