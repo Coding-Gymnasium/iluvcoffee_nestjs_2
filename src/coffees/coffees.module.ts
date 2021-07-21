95e4ff6609bf93a013e96d05b913968c56011d91
Nico Rithner <nico@altvia.com>
2021-07-21 13:06:42 -0600

    Complete 'Class Providers' lesson
    Co-authored-by: Nico nicorithner@gmail.com

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -7,11 +7,22 @@ import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from '../events/entities/event.entity';
 
+class ConfigService {}
+class DevelopmentConfigService {}
+class ProductionConfigService {}
+
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])], // 👈 Adding Coffee Entity here to TypeOrmModule.forFeature
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
+    {
+      provide: ConfigService,
+      useClass:
+        process.env.NODE_ENV === 'development'
+          ? DevelopmentConfigService
+          : ProductionConfigService,
+    },
     {
       provide: COFFEE_BRANDS,
       useValue: ['buddy brew', 'nescafe'],
